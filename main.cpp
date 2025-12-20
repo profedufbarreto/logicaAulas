c393027a17b42f604f0353cf8a7bdd03208907e0
profedufbarreto <01289211027@senacrs.edu.br>
2025-12-20 15:55:28 -0300

    for

--- main.cpp
+++ main.cpp
@@ -2,14 +2,14 @@
 
 int main(){
 
-    int var;
+    int tab, resultado;
 
-    std::cout<<"Digite um valor para var: ";
-    std::cin>>var;
+    std::cout<<"Digite um valor para tabuada: ";
+    std::cin>>tab;
 
-    for(int i = 0; i <= var; i++){
-        std::cout<<"Eu estou aprendendo a fazer loopings!"<<std::endl;
-        i++;
+    for(int i = 0; i <= 10; i++){
+        resultado = i * tab;
+        std::cout<<"O resultado de: "<<i<<" * "<<tab<<" = "<<resultado<<std::endl;
     }
 
     return 0;
