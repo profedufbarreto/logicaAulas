e5cdae19859c50f6962212bd8e4b974e5bb8db47
profedufbarreto <01289211027@senacrs.edu.br>
2025-12-20 15:27:28 -0300

    for

--- main.cpp
+++ main.cpp
@@ -1,65 +1,16 @@
 #include<iostream>
 
-int somar(int a, int b){
-    return a + b;
-}
-
-int subtrair(int a, int b){
-    return a - b;
-}
-
-int multiplicar(int a, int b){
-    return a * b;
-}
-
-float dividir(int a, int b){
-    if(b <= 0){
-        std::cout<<"Não pode ser dividido por número menor ou igual que 0!"<<std::endl;
-    }else{
-        return a / b;
-    }
-}
-
 int main(){
 
-    int a, b, opcao;
-    float resultado;
+    int var;
 
-    std::cout<<"Digite um valor para a: ";
-    std::cin>>a;
+    std::cout<<"Digite um valor para var: ";
+    std::cin>>var;
 
-    std::cout<<"Digite um valor para b: ";
-    std::cin>>b;
-
-    std::cout<<"Digite a opção: "<<std::endl;
-    std::cout<<"1 - Soma"<<std::endl;
-    std::cout<<"2 - Subtração"<<std::endl;
-    std::cout<<"3 - Multiplicação"<<std::endl;
-    std::cout<<"4 - Divisão"<<std::endl;
-    std::cin>>opcao;
-
-    switch(opcao){
-        case 1:
-        resultado = somar(a, b);
-        std::cout<<"O resultado da soma é: "<<resultado<<std::endl;
-        break;
-        case 2:
-        resultado = subtrair(a, b);
-        std::cout<<"O resultado da subtração é: "<<resultado<<std::endl;
-        break;
-        case 3:
-        resultado = multiplicar(a, b);
-        std::cout<<"O resultado da multiplicação é: "<<resultado<<std::endl;
-        break;
-        case 4:
-        resultado = dividir(a, b);
-        std::cout<<"O resultado da divisão é: "<<resultado<<std::endl;
-        break;
-        default:
-        std::cout<<"Você digitou um valor inválido!"<<std::endl;
-        break;
+    for(int i = 0; i <= var; i++){
+        std::cout<<"Eu estou aprendendo a fazer loopings!"<<std::endl;
+        i++;
     }
-
 
     return 0;
 }
