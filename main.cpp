bb26ee3792e5b536d902326bcfd5265fa8d970a8
profedufbarreto <01289211027@senacrs.edu.br>
2025-12-22 14:20:15 -0300

    do while

--- main.cpp
+++ main.cpp
@@ -1,23 +1,15 @@
 #include<iostream>
 
-void tabuada(int tab){
-    int numeros[11] = {0,1,2,3,4,5,6,7,8,9,10};
-    for(int i = 0; i <= 10; i++){
-        int resultado = tab * numeros[i];
-        std::cout<<"O resultado é: "<<tab<<" * "<<numeros[i]<<" = "<<resultado<<std::endl;
-    }
-}
-
 int main(){
 
-    int tab;
+    int idade;
 
-    std::cout<<"Digite a tabuada desejada: ";
-    std::cin>>tab;
+    do{
+        std::cout<<"Digite a sua idade: "<<std::endl;
+        std::cin>>idade;
+    }while(idade >=0 && idade <= 110);
 
-    tabuada(tab);
-
-
+    std::cout<<"Digite uma idade válida!"<<std::endl;
 
     return 0;
 }
