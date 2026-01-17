eaef4d3dcf64450ae6d14ad0b89168d468b6398c
EDUARDO FILIPPSEN BARRETO <01289211027@senacrs.edu.br>
2026-01-17 11:31:47 -0300

    usando limits e getline

--- main.cpp
+++ main.cpp
@@ -1,55 +1,21 @@
 #include<iostream>
-#include<iomanip>
-#include<string>
 #include<limits>
-
-void cobrarCombustivel(const std::string& nome, double precoPorLitro){
-    double litros;
-    std::cout<<"Quantidade abastecida (litros): "<<std::endl;
-
-    if(!(std::cin >> litros)){
-        std::cin.clear();
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        std::cout<<"Entrada inválida. Tente novamente. \n";
-        return;
-    }
-
-    if(litros <= 0.0){
-        std::cout<<"Quantidade deve ser maior que zero.\n";
-        return;
-    }
-
-    double total = precoPorLitro * litros;
-
-    std::cout<<std::fixed<<std::setprecision(2);
-    std::cout<<"Seu abastecimento em "<<nome
-             <<" foi de R$: "<<total<<'\n';
-
-}
+#include<string>
 
 int main(){
 
-    std::cout<<std::fixed<<std::setprecision(2);
+    int idade;
+    std::string nome;
 
-    constexpr double precoGasolina = 6.09;
-    constexpr double precoEtanol = 4.79;
-    constexpr double precoDiesel = 5.99;
+    std::cout<<"Qual sua idade: "<<std::endl;
+    std::cin>>idade;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
-    int opcao;
+    std::cout<<"Qual seu nome: "<<std::endl;
+    std::getline(std::cin, nome);
 
-    std::cout<<"------------\n";
-    std::cout<<"1 - GASOLINA (R$ " <<precoGasolina<<")\n";
-    std::cout<<"2 - ETANOL (R$ "<<precoEtanol<<")\n";
-    std::cout<<"3 - DIESEL (R$ "<<precoDiesel<<")\n";
-    std::cout<<"------------\n";
-    std::cin>>opcao;
-
-    switch(opcao){
-        case 1: cobrarCombustivel("GASOLINA", precoGasolina); break;
-        case 2: cobrarCombustivel("ETANOL", precoEtanol); break;
-        case 3: cobrarCombustivel("DIESEL", precoDiesel); break;
-        default: std::cout<<"Opção inválida!\n";
-    }
+    std::cout<<"Seu nome é: "<<nome<<std::endl;
+    std::cout<<"Sua idade é: "<<idade<<std::endl;
 
     return 0;
 }
