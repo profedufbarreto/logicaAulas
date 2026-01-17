d413db3720005a2616816914dc066835bbe5f8d0
EDUARDO FILIPPSEN BARRETO <01289211027@senacrs.edu.br>
2026-01-17 10:58:54 -0300

    versão fraca

--- main.cpp
+++ main.cpp
@@ -1,27 +1,57 @@
 #include<iostream>
-#include<math.h>
+#include<string.h>
+#include<iomanip>
 
-void lerDiametro(double &r){
-    
-    std::cout<<"Digite o raio do círculo: "<<std::endl;
-    std::cin>>r;
-    
+void gasolina(){
+    float litros, total;
+    const float valor = 6.09;
+    std::cout<<"Quantidade abastecida (litros): "<<std::endl;
+    std::cin>>litros;
+    total = valor * litros;
+    std::cout<<"Seu abastecimento em GASOLINA foi de R$: "<<std::fixed
+        <<std::setprecision(2)<<total<<std::endl;
 }
 
-void calcularDiametro(){
-    double raio, diametro;
+void etanol(){
+    float litros, total;
+    const float valor = 4.79;
+    std::cout<<"Quantidade abastecida (litros): "<<std::endl;
+    std::cin>>litros;
+    total = valor * litros;
+    std::cout<<"Seu abastecimento em ETANOL foi de R$: "<<std::fixed
+        <<std::setprecision(2)<<total<<std::endl;
+}
 
-    lerDiametro(raio);
-
-    diametro = pow(raio, 2);
-
-    std::cout<<"O valor do diâmetro é de: "<<diametro<<std::endl;
-
+void diesel(){
+    float litros, total;
+    const float valor = 5.99;
+    std::cout<<"Quantidade abastecida (litros): "<<std::endl;
+    std::cin>>litros;
+    total = valor * litros;
+    std::cout<<"Seu abastecimento em DIESEL foi de R$: "<<std::fixed
+        <<std::setprecision(2)<<total<<std::endl;
 }
 
 int main(){
 
-    calcularDiametro();
+    int comb;
+
+    std::cout<<"-------------"<<std::endl;
+    std::cout<<"1 - GASOLINA"<<std::endl;
+    std::cout<<"2 - ETANOL"<<std::endl;
+    std::cout<<"3 - DIESEL"<<std::endl;
+    std::cout<<"-------------"<<std::endl;
+    std::cin>>comb;
+
+    if(comb == 1){
+        gasolina();
+    }else if(comb ==2){
+        etanol();
+    }else if(comb == 3){
+        diesel();
+    }else{
+        std::cout<<"Digitação incorreta!"<<std::endl;
+    }
 
     return 0;
 }
