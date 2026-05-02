a2673b432c70708f2077129adb85f1990b837545
profedufbarreto <01289211027@senacrs.edu.br>
2026-05-02 11:32:23 -0300

    calculadora simples com funções

--- main.c
+++ main.c
@@ -1,12 +1,66 @@
 #include <stdio.h>
 
-void ola(){
-    printf("Olá mundo!!");
+int somar(int a, int b){
+    return a + b;
+}
+
+int subtrair(int a, int b){
+    return a - b;
+}
+
+int multiplicar(int a, int b){
+    return a * b;
+}
+
+float dividir(float a, float b){
+    if(a <= 0){
+        printf("Digite um valor maior que 0!");
+    }else{
+        return a / b;
+    }
 }
 
 int main() {
     
-    ola();
+    int num1, num2;
+    int resultado;
+    int opcao;
+
+    printf("----- Bem-vindos a uma simples calculadora em C: -----");
+    printf("\nEscolha um valor para opção: ");
+    printf("\n1 - SOMA: ");
+    printf("\n2 - Subtração: ");
+    printf("\n3 - Multiplicação: ");
+    printf("\n4 - Divisão: ");
+    scanf("%d", &opcao);
+
+    printf("Digite um valor para num1: ");
+    scanf("%d", &num1);
+
+    printf("Digite um valor para num2: ");
+    scanf("%d", &num2);
+
+    switch(opcao){
+        case 1:
+            resultado = somar(num1, num2);
+            printf("O resultado é %d\n", resultado);
+        break;
+        case 2:
+            resultado = subtrair(num1, num2);
+            printf("O resultado é %d\n", resultado);
+        break;
+        case3:
+            resultado = multiplicar(num1, num2);
+            printf("O resultado é %d\n", resultado);
+        break;
+        case 4: 
+            resultado = dividir(num1, num2);
+            printf("O resultado é %d\n", resultado);
+        break;
+        default:
+            printf("Você digitou um valor inválido!");
+        break;
+    }
 
     return 0;
 }
