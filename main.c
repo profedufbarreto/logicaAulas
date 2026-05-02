cbc7011c70232e4a281a56b1801d3e38e30d2911
profedufbarreto <01289211027@senacrs.edu.br>
2026-05-02 16:08:38 -0300

    exercício para fazer em C++ com pega ratão

--- main.c
+++ main.c
@@ -1,27 +1,29 @@
 #include<stdio.h>
 
-
-
 int main(){
 
-    int nivelCombustivel;
+    float distancia, consumo, precoLitro, litros;
+    float custoTotal;
 
-    printf("Qual nível de combustível: ");
-    scanf("%d", &nivelCombustivel);
+    printf("Qual a distância da viagem: ");
+    scanf("%f", &distancia);
 
-    if(nivelCombustivel < 0){
-        return 1;
+    printf("Qual o consumo médio do veículo: ");
+    scanf("%f", &consumo);
+
+    printf("Qual valor do litro do combustível: ");
+    scanf("%f", &precoLitro);
+
+    litros = distancia / consumo;
+    custoTotal = litros * precoLitro;
+
+    if(custoTotal >= 100){
+        printf("\nViagem cara! Considere dividir os custos!");
+        (distancia >= 100) ? printf("\nViagem longa!") : printf("\nViagem curta!");
+        printf("\nViagem custará %.2f\n", custoTotal);
     }else{
-        if(nivelCombustivel < 10){
-            printf("\nALERTA: Nível crítico! Abasteça agora!");
-            (nivelCombustivel < 30) ? printf("\nLuz acesa!") : printf("\nLuz apagada!");
-        }else if(nivelCombustivel < 30){
-            printf("\nAtenção! Combustível na reserva!");
-            (nivelCombustivel < 30) ? printf("\nLuz acesa!") : printf("\nLuz apagada!");
-        }else{
-            printf("\nNível seguro para viajar!");
-            (nivelCombustivel < 30) ? printf("\nLuz acesa!") : printf("\nLuz apagada!");
-        }
+        printf("\nO custo está dentro do orçamento!");
+        (distancia >= 100) ? printf("\nViagem longa!") : printf("\nViagem curta!");
     }
 
     return 0;
