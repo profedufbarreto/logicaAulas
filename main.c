e80a295b04f73cf5055fc1f9e99a787e1ad56810
profedufbarreto <01289211027@senacrs.edu.br>
2026-05-02 15:47:58 -0300

    sistema de combustível

--- main.c
+++ main.c
@@ -1,10 +1,28 @@
 #include<stdio.h>
 
+
+
 int main(){
 
-    int num = 6;
+    int nivelCombustivel;
 
-    (num >= 5) ? printf("Verdade!") : printf("Mentira!");
+    printf("Qual nível de combustível: ");
+    scanf("%d", &nivelCombustivel);
+
+    if(nivelCombustivel < 0){
+        return 1;
+    }else{
+        if(nivelCombustivel < 10){
+            printf("\nALERTA: Nível crítico! Abasteça agora!");
+            (nivelCombustivel < 30) ? printf("\nLuz acesa!") : printf("\nLuz apagada!");
+        }else if(nivelCombustivel < 30){
+            printf("\nAtenção! Combustível na reserva!");
+            (nivelCombustivel < 30) ? printf("\nLuz acesa!") : printf("\nLuz apagada!");
+        }else{
+            printf("\nNível seguro para viajar!");
+            (nivelCombustivel < 30) ? printf("\nLuz acesa!") : printf("\nLuz apagada!");
+        }
+    }
 
     return 0;
 }
