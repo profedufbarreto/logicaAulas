925c9082d9d8a717230d22f939fb075883db9a55
profedufbarreto <01289211027@senacrs.edu.br>
2026-05-06 20:23:44 -0300

    exercitando funções

--- main.cpp
+++ main.cpp
@@ -1,17 +1,21 @@
 #include<iostream>
 #include<string>
 
-void dizerNome(std::string n){
+void dizerNome(std::string n, int a){
     std::cout<<"Digite o seu nome: "<<std::endl;
     std::cin>>n;
+    std::cout<<"Digite sua idade: "<<std::endl;
+    std::cin>>a;
     std::cout<<"Seu nome é: "<<n<<std::endl;
+    std::cout<<"Você tem "<<a<<" anos."<<std::endl;
 }
 
 int main(){
 
     std::string nome;
+    int age;
 
-    dizerNome(nome);
+    dizerNome(nome, age);
 
     return 0;
 }
