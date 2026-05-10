de0048af086ff362065e16c2e7857bdf02eea094
profedufbarreto <01289211027@senacrs.edu.br>
2026-05-10 12:14:40 -0300

    puxando de outra função

--- main.cpp
+++ main.cpp
@@ -4,21 +4,13 @@ int somar(int a, int b){
     return a + b;
 }
 
+void exibirResultado(int a, int b){
+    std::cout<<somar(a, b)<<std::endl;
+}
+
 int main(){
 
-    int num1;
-    int num2;
-    int resultado;
-
-    std::cout<<"Digite um valor para num1: "<<std::endl;
-    std::cin>>num1;
-
-    std::cout<<"Digite um valor para num2: "<<std::endl;
-    std::cin>>num2;
-
-    resultado = somar(num1, num2);
-
-    std::cout<<"O resultado será de: "<<resultado<<std::endl;
+   exibirResultado(3, 2);
 
     return 0;
 }
