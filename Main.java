bd4b6a156e7559b6e17d861879e55c7b3a8d215d
profedufbarreto <01289211027@senacrs.edu.br>
2026-05-14 21:18:39 -0300

    java class

--- Main.java
+++ Main.java
@@ -1,68 +1,15 @@
+abstract  class Funcionario{
+    protected String nome;
+    protected double salarioBase;
+    protected String departamento;
 
-import java.util.Scanner;
-
-class ContaBancaria{
-    private String titular;
-    private double saldo;
-
-    public ContaBancaria(String nome, double saldoInicial){
-        this.titular = nome;
-        this.saldo = saldoInicial;
+    public Funcionario(){
+        nome = "";
+        salarioBase = 0;
+        departamento = "";
     }
 
-    public void depositar(){
-        double valor;
-        System.out.print("Digite o valor a depositar: ");
-        Scanner sc = new Scanner(System.in);
-        valor = sc.nextDouble();
+    public abstract double calcularSalario();
 
-        if(valor > 0){
-            saldo += valor;
-            System.out.println("Depósito realizado!");
-        }else{
-            System.out.println("Valor inválido!");
-        }
-    }
 
-    public void sacar(){
-        double valor;
-        System.out.print("Digite o valor a sacar: ");
-        Scanner sc = new Scanner(System.in);
-        valor = sc.nextDouble();
-
-        if(valor > 0 && valor <= saldo){
-            saldo -= valor;
-            System.out.println("Saque realizado!");
-        }else{
-            System.out.println("Saldo insuficiente!");
-        }
-    }
-
-    public void exibirSaldo(){
-        System.out.println(titular + " -- Saldo: R$ " + saldo);
-    }
-}
-
-public class Main{
-    public static void main(String[] args) {
-        
-        String nome;
-        double saldoInicial;
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Digite aqui o nome do titular: ");
-
-        nome = sc.nextLine();
-
-        System.out.print("Digite aqui o saldo inicial: ");
-        Scanner sc = new Scanner(System.in);
-        saldoInicial = sc.nextDouble();
-
-        ContaBancaria conta = new ContaBancaria(nome, saldoInicial);
-
-        sc.close();
-
-        return;
-    }
 }
--- main.cpp
+++ main.cpp
@@ -1,47 +1,16 @@
 #include<iostream>
 #include<string>
 
-class Carro{ //classe
-    private:
-    std::string marca; //atributo
-    std::string modelo; //atributo
-    int velocidade; //atributo
-
+class Animal{
+    protected:
+        std::string nome;
+        std::string tipo;
+    
     public:
-    Carro(std::string marca, std::string modelo, int velocidade){ //construtor
-        this->marca = marca;
-        this->modelo = modelo;
-        this->velocidade = velocidade;
-    }
+        Animal(){
+            nome = "";
+            tipo = "";
+        }
 
-    void cadastrar(){ //método
-        std::cout<<"Qual a marca: "<<std::endl;
-        std::cin>>marca;
-
-        std::cout<<"Qual o modelo: "<<std::endl;
-        std::cin>>modelo;
-
-        std::cout<<"Velocidade máxima: "<<std::endl;
-        std::cin>>velocidade;
-    }
-
-    void exibirInfo(){ //método
-        std::cout<<"A marca é: "<<marca<<std::endl;
-        std::cout<<"O modelo é: "<<modelo<<std::endl;
-        std::cout<<"A velocidade máxima é: "<<velocidade<<" Km/h"<<std::endl;
-    }
-};
-
-int main(){
-
-    Carro c1("Toyota", "Hillux", 140);
-    Carro c2("Mitsubishi", "L200 Triton", 138);
-
-
-    // c1.cadastrar();
-    // c2.cadastrar();
-    c1.exibirInfo();
-    c2.exibirInfo();
-
-    return 0;
+        
 }
