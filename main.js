7714058eac0f822b98567669f1d22b7318cc66b9
profedufbarreto <01289211027@senacrs.edu.br>
2026-05-23 10:47:30 -0300

    if, else

--- main.js
+++ main.js
@@ -1,30 +1,8 @@
 "use strict";
-function somar(x, y) {
-    return x + y;
+let age = 38;
+if (age >= 18) {
+    console.log("Você é maior de idade!");
 }
-function subtrair(x, y) {
-    return x - y;
-}
-function multiplicar(x, y) {
-    return x * y;
-}
-function dividir(x, y) {
-    return x / y;
-}
-let opcao = 2;
-switch (opcao) {
-    case 1:
-        console.log(somar(10, 5));
-        break;
-    case 2:
-        console.log(subtrair(10, 6));
-        break;
-    case 3:
-        console.log(multiplicar(5, 5));
-        break;
-    case 4:
-        console.log(dividir(10, 2));
-        break;
-    default:
-        console.log("Você escolheu uma opção inválida!!");
+else {
+    console.log("Você é menor de idade!");
 }
--- main.ts
+++ main.ts
@@ -1,34 +1,7 @@
-function somar(x: number, y: number): number{
-    return x + y;
-}
+let age: number = 38;
 
-function subtrair(x: number, y: number): number{
-    return x - y;
-}
-
-function multiplicar(x: number, y: number): number{
-    return x * y;
-}
-
-function dividir(x: number, y: number): number{
-    return x / y;
-}
-
-let opcao: number = 2;
-
-switch(opcao){
-    case 1:
-        console.log(somar(10, 5));
-        break;
-    case 2:
-        console.log(subtrair(10, 6));
-        break;
-    case 3:
-        console.log(multiplicar(5, 5));
-        break;
-    case 4:
-        console.log(dividir(10, 2));
-        break;
-    default:
-        console.log("Você escolheu uma opção inválida!!");
+if(age >= 18){
+    console.log("Você é maior de idade!");
+}else{
+    console.log("Você é menor de idade!");
 }
