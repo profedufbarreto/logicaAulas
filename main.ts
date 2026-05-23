7c1cbb72d85721661b1e326758673426e60d10b4
profedufbarreto <01289211027@senacrs.edu.br>
2026-05-23 14:40:23 -0300

    abstração

--- main.ts
+++ main.ts
@@ -1,35 +1,33 @@
-class Pessoa{ 
-    public nome: string;
-    private idade: number;
-    protected cpf: string;
+abstract class Animal{
+    nome: string;
 
-    constructor(nome: string, idade: number, cpf: string){
+    constructor(nome: string){
         this.nome = nome;
-        this.idade = idade;
-        this.cpf = cpf;
     }
 
-    public getIdade(): number{
-        return this.idade;
-    }
+    abstract fazerSom(): void; //filho OBRIGA implementar;
 
-    public getCpf(): string{
-        return this.cpf;
-    }
-
-    public setIdade(novaIdade: number): void{
-        if(novaIdade > 0){
-            this.idade = novaIdade;
-        }
-    }
-
-    public saudacao(): void{
-        console.log(`Olá, eu sou o ${this.nome}.`);
+    mostrarNome(): void{
+        console.log(`Nome: ${this.nome}.`);
     }
 }
 
-let p1 = new Pessoa("Eduardo", 38, "123.456.789-01");
+class Cachorro extends Animal{
+    fazerSom(): void {
+        console.log(`${this.nome} faz: Au au!!`);
+    }
+}
 
-console.log(p1.nome);
-console.log(p1.getIdade());
-console.log(p1.getCpf());
+class Gato extends Animal{
+    fazerSom(): void {
+        console.log(`${this.nome} faz: miauuu!!`);
+    }
+}
+
+let animal = new Animal("Rex");
+
+let dog = new Cachorro("Rex");
+dog.fazerSom();
+
+let cat = new Gato("Mimi");
+cat.fazerSom();
