505aa362eab4cabd1976ccb6aa8e574cf483949c
profedufbarreto <01289211027@senacrs.edu.br>
2026-05-23 14:10:09 -0300

    classe com herança

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
 "use strict";
-class Pessoa {
-    constructor(nome, idade) {
+class Animal {
+    constructor(nome) {
         this.nome = nome;
-        this.idade = idade;
     }
-    saudacao() {
-        console.log(`Olá, meu nome é ${this.nome}`);
-    }
-    mostrarIdade() {
-        console.log(`Tenho ${this.idade} anos`);
+    fazerSom() {
+        console.log(`${this.nome} faz um som!`);
     }
 }
-let pessoa1 = new Pessoa("Eduardo", 38);
-pessoa1.saudacao();
-pessoa1.mostrarIdade();
-let pessoa2 = new Pessoa("Bianca", 35);
-pessoa2.saudacao();
-pessoa2.mostrarIdade();
+class Cachorro extends Animal {
+    fazerSom() {
+        console.log(`${this.nome} faz: Au au!!`);
+    }
+}
+class Gato extends Animal {
+    fazerSom() {
+        console.log(`${this.nome} faz: Miauuu!!`);
+    }
+}
+let dog = new Cachorro("Rex");
+dog.fazerSom();
+let cat = new Gato("Mimi");
+cat.fazerSom();
--- main.ts
+++ main.ts
@@ -1,25 +1,29 @@
-class Pessoa {
+class Animal {
     nome: string;
-    idade: number;
 
-    constructor(nome: string, idade: number){
+    constructor(nome: string){
         this.nome = nome;
-        this.idade = idade;
     }
 
-    saudacao(): void{
-        console.log(`Olá, meu nome é ${this.nome}`);
-    }
-
-    mostrarIdade(): void{
-        console.log(`Tenho ${this.idade} anos`);
+    fazerSom(): void{
+        console.log(`${this.nome} faz um som!`);
     }
 }
 
-let pessoa1 = new Pessoa("Eduardo", 38);
-pessoa1.saudacao();
-pessoa1.mostrarIdade();
+class Cachorro extends Animal {
+    fazerSom(): void {
+        console.log(`${this.nome} faz: Au au!!`);
+    }
+}
 
-let pessoa2 = new Pessoa("Bianca", 35);
-pessoa2.saudacao();
-pessoa2.mostrarIdade();
+class Gato extends Animal{
+    fazerSom(): void {
+        console.log(`${this.nome} faz: Miauuu!!`);
+    }
+}
+
+let dog = new Cachorro("Rex");
+dog.fazerSom();
+
+let cat = new Gato("Mimi");
+cat.fazerSom();
