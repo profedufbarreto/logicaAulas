aa9e8fa534ff811b3e999ffa8df1f1d905ae7e61
profedufbarreto <01289211027@senacrs.edu.br>
2026-05-23 14:00:50 -0300

    classes com class

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 "use strict";
-let pPessoa = {
-    nome: "Eduardo",
-    idade: 38
-};
-console.log(`Meu nome é: ${pPessoa.nome}`);
-console.log(`E tenho ${pPessoa.idade} anos.`);
+class Pessoa {
+    constructor(nome, idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+    saudacao() {
+        console.log(`Olá, meu nome é ${this.nome}`);
+    }
+    mostrarIdade() {
+        console.log(`Tenho ${this.idade} anos`);
+    }
+}
+let pessoa1 = new Pessoa("Eduardo", 38);
+pessoa1.saudacao();
+pessoa1.mostrarIdade();
+let pessoa2 = new Pessoa("Bianca", 35);
+pessoa2.saudacao();
+pessoa2.mostrarIdade();
--- main.ts
+++ main.ts
@@ -1,12 +1,25 @@
-interface Pessoa{
+class Pessoa {
     nome: string;
     idade: number;
+
+    constructor(nome: string, idade: number){
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    saudacao(): void{
+        console.log(`Olá, meu nome é ${this.nome}`);
+    }
+
+    mostrarIdade(): void{
+        console.log(`Tenho ${this.idade} anos`);
+    }
 }
 
-let pPessoa: Pessoa = {
-    nome: "Eduardo",
-    idade: 38
-};
+let pessoa1 = new Pessoa("Eduardo", 38);
+pessoa1.saudacao();
+pessoa1.mostrarIdade();
 
-console.log(`Meu nome é: ${pPessoa.nome}`);
-console.log(`E tenho ${pPessoa.idade} anos.`);
+let pessoa2 = new Pessoa("Bianca", 35);
+pessoa2.saudacao();
+pessoa2.mostrarIdade();
