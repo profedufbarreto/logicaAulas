65ae541513ba7bad1d2ec3f57db457066e5b0a45
profedufbarreto <01289211027@senacrs.edu.br>
2026-05-23 10:50:32 -0300

    if, else if, else

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 "use strict";
 let age = 38;
-if (age >= 18) {
-    console.log("Você é maior de idade!");
+if (age < 0) {
+    console.log("Idade inválida!");
+}
+else if (age < 6) {
+    console.log("Criança!");
+}
+else if (age < 10) {
+    console.log("Pré-adolescente!");
+}
+else if (age < 16) {
+    console.log("Adolescente!");
 }
 else {
-    console.log("Você é menor de idade!");
+    console.log("Adulto!");
 }
--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
 let age: number = 38;
 
-if(age >= 18){
-    console.log("Você é maior de idade!");
+if(age < 0){
+    console.log("Idade inválida!");
+}else if(age < 6){
+    console.log("Criança!");
+}else if(age < 10){
+    console.log("Pré-adolescente!");
+}else if(age < 16){
+    console.log("Adolescente!");
 }else{
-    console.log("Você é menor de idade!");
+    console.log("Adulto!");
 }
