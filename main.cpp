3aecf7ec7309e8e79353c6c50521f4996072e714
profedufbarreto <01289211027@senacrs.edu.br>
2026-05-26 21:50:46 -0300

    tupla

--- main.cpp
+++ main.cpp
@@ -1,30 +1,17 @@
 #include <iostream>
-
-bool ehPrimo(int numero) {
-    if (numero <= 1) {
-        return false;
-    }
-    
-    for (int i = 2; i < numero; i++) {
-        if (numero % i == 0) {
-            return false;
-        }
-    }
-    
-    return true;
-}
+#include <tuple>   // Biblioteca necessária
+#include <string>
 
 int main() {
-    int numero;
-    
-    std::cout << "Digite um número: ";
-    std::cin >> numero;
-    
-    if (ehPrimo(numero)) {
-        std::cout << numero << " é primo!" << std::endl;
-    } else {
-        std::cout << numero << " NÃO é primo!" << std::endl;
-    }
-    
+    // 1. Criando uma tupla: id (int), nome (string), nota (double)
+    std::tuple<int, std::string, double> aluno(1, "Eduardo", 9.5);
+
+    // 2. Acessando os dados usando std::get<índice>(nome_da_tupla)
+    // O índice precisa ser conhecido em tempo de compilação
+    std::cout << "ID: " << std::get<0>(aluno) << "\n";
+    std::cout << "Nome: " << std::get<1>(aluno) << "\n";
+    std::cout << "Nota: " << std::get<2>(aluno) << "\n";
+
+
     return 0;
 }
