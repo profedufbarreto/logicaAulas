822f1764efe8e0eb480bdaebd816eb99a0899880
profedufbarreto <01289211027@senacrs.edu.br>
2026-05-28 20:52:26 -0300

    bubble sort

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@ int main() {
     for (int i = 0; i < 5; i++) std::cout << numeros[i] << " ";
     std::cout << "\n\n";
     
-    // Bubble sort
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j < 4; j++) {
             if (numeros[j] > numeros[j+1]) {
