192ca09232c196db1c340bf9724732589b66b66e
profedufbarreto <01289211027@senacrs.edu.br>
2026-06-02 20:24:56 -0300

    ponteiro

--- main.cpp
+++ main.cpp
@@ -1,37 +1,11 @@
 #include <iostream>
-#include <string>
-#include <cctype>
-
-bool ehPalindromo(std::string s) {
-    int inicio = 0;
-    int fim = s.length() - 1;
-    
-    while (inicio < fim) {
-        if (s[inicio] != s[fim]) {
-            return false;
-        }
-        inicio++;
-        fim--;
-    }
-    return true;
-}
 
 int main() {
-    std::string palavra;
+    int numero = 42;
     
-    std::cout << "Digite uma palavra: ";
-    std::cin >> palavra;
-    
-    // Converter para minúsculas
-    for (int i = 0; i < palavra.length(); i++) {
-        palavra[i] = std::tolower(palavra[i]);
-    }
-    
-    if (ehPalindromo(palavra)) {
-        std::cout << "É palíndromo!" << std::endl;
-    } else {
-        std::cout << "Não é palíndromo!" << std::endl;
-    }
+    // & significa "endereço de"
+    std::cout << "Número: " << numero << std::endl;
+    std::cout << "Endereço: " << &numero << std::endl;
     
     return 0;
 }
