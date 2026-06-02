9498718bef82f20c977f3cbf7c09ac916020a106
profedufbarreto <01289211027@senacrs.edu.br>
2026-06-02 19:12:59 -0300

    palindromo

--- main.cpp
+++ main.cpp
@@ -1,26 +1,21 @@
 #include <iostream>
+#include <string>
 
-int contarDigitos(int n) {
-    // PARADA: Se o número chegou a zero, não há mais dígitos para contar
-    if (n == 0) return 0;
+bool ehPalindromo(std::string s) {
+    int inicio = 0;
+    int fim = s.length() - 1;
     
-    // RECURSÃO: Soma 1 (pelo dígito atual) e passa o resto do número para a próxima chamada
-    return 1 + contarDigitos(n / 10);
+    while (inicio < fim) {
+        if (s[inicio] != s[fim]) {
+            return false;
+        }
+        inicio++;
+        fim--;
+    }
+    return true;
 }
 
 int main() {
-    int numeroUsuario; // 1. Cria a variável vazia para armazenar o valor
-    
-    std::cout << "Digite um numero inteiro: ";
-    std::cin >> numeroUsuario; // 2. Captura o que o usuário digitar no teclado
-    
-    // Tratamento para o caso específico do número zero (0 tem 1 dígito)
-    if (numeroUsuario == 0) {
-        std::cout << "O numero 0 tem 1 digito.\n";
-    } else {
-        // 3. Passa a variável preenchida para a função calcular
-        std::cout << "O numero " << numeroUsuario << " tem " << contarDigitos(numeroUsuario) << " digitos.\n";
-    }
-    
+    std::cout << ehPalindromo("arar") << std::endl;
     return 0;
 }
