c6f78cbb98fb7eb11971096b288cdd1ca35a9dbe
profedufbarreto <01289211027@senacrs.edu.br>
2026-06-02 20:59:55 -0300

    struct

--- main.cpp
+++ main.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
+#include <string>
+
+struct Pessoa {
+    std::string nome;
+    int idade;
+    float altura;
+};
 
 int main() {
-    int numero = 42;
-    int* ptr = &numero;
+    // Criar uma pessoa
+    Pessoa p1;
     
-    std::cout << "Antes: " << numero << std::endl;
+    p1.nome = "João";
+    p1.idade = 20;
+    p1.altura = 1.75;
     
-    *ptr = 99;  // Modifica o valor através do ponteiro
-    
-    std::cout << "Depois: " << numero << std::endl;
+    // Acessar os dados
+    std::cout << "Nome: " << p1.nome << std::endl;
+    std::cout << "Idade: " << p1.idade << std::endl;
+    std::cout << "Altura: " << p1.altura << std::endl;
     
     return 0;
 }
