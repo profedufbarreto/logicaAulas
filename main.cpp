bd673aa285d9d06f78a8c66c1c771972fb528c01
profedufbarreto <01289211027@senacrs.edu.br>
2026-06-02 20:27:50 -0300

    alterando com ponteiro

--- main.cpp
+++ main.cpp
@@ -4,9 +4,11 @@ int main() {
     int numero = 42;
     int* ptr = &numero;
     
-    // * significa "conteúdo do endereço"
-    std::cout << "Acessar via variável: " << numero << std::endl;
-    std::cout << "Acessar via ponteiro: " << *ptr << std::endl;
+    std::cout << "Antes: " << numero << std::endl;
+    
+    *ptr = 99;  // Modifica o valor através do ponteiro
+    
+    std::cout << "Depois: " << numero << std::endl;
     
     return 0;
 }
