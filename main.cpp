7427fd276e1e9687cdcba2f97d3770055dafb643
profedufbarreto <01289211027@senacrs.edu.br>
2026-06-11 21:10:01 -0300

    horner

--- main.cpp
+++ main.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 
-int encontrarMaior(int* arr, int tamanho) {
-    int maior = arr[0];
-    for (int i = 1; i < tamanho; i++) {
-        if (arr[i] > maior) {
-            maior = arr[i];
-        }
+int horner(int* coeficientes, int grau, int x) {
+    int resultado = coeficientes[grau];
+    
+    for (int i = grau - 1; i >= 0; i--) {
+        resultado = resultado * x + coeficientes[i];
     }
-    return maior;
+    
+    return resultado;
 }
 
 int main() {
-    int arr[] = {3, 7, 2, 9, 1, 5};
-    std::cout << "Maior elemento: " << encontrarMaior(arr, 6) << std::endl;  // 9
+    // p(x) = 2x^3 + 3x^2 + 4x + 5
+    int coef[] = {5, 4, 3, 2};  // índice 0 = constante
+    
+    std::cout << "p(2) = " << horner(coef, 3, 2) << std::endl;  // 41
+    
     return 0;
 }
