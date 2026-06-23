c0502b818f7c6b5a6a427e76208d482bc8760242
profedufbarreto <01289211027@senacrs.edu.br>
2026-06-23 19:46:25 -0300

    pilha com menu

--- main.cpp
+++ main.cpp
@@ -1,27 +1,56 @@
 #include <iostream>
 
 int main() {
-    int fila[5];
-    int inicio = 0;
-    int fim = 0;
+    int pilha[10];
+    int topo = -1;
+    int opcao;
     
-    std::cout << "===== FILA =====" << std::endl;
-    std::cout << std::endl;
+    std::cout << "===== PILHA COM MENU =====" << std::endl;
     
-    // ENFILEIRAR
-    fila[0] = 100;
-    fila[1] = 200;
-    fila[2] = 300;
-    fim = 3;
-    
-    std::cout << "Elementos enfileirados: 100, 200, 300" << std::endl;
-    std::cout << std::endl;
-    
-    // DESENFILEIRAR
-    std::cout << "Desenfileirando..." << std::endl;
-    while (inicio < fim) {
-        std::cout << "Removido: " << fila[inicio] << std::endl;
-        inicio++;
+    while (true) {
+        std::cout << std::endl;
+        std::cout << "1. Empilhar" << std::endl;
+        std::cout << "2. Desempilhar" << std::endl;
+        std::cout << "3. Exibir pilha" << std::endl;
+        std::cout << "4. Sair" << std::endl;
+        std::cout << "Escolha: ";
+        std::cin >> opcao;
+        
+        if (opcao == 1) {
+            if (topo < 9) {
+                int valor;
+                std::cout << "Digite o valor: ";
+                std::cin >> valor;
+                topo++;
+                pilha[topo] = valor;
+                std::cout << "Empilhado!" << std::endl;
+            } else {
+                std::cout << "Pilha cheia!" << std::endl;
+            }
+        } 
+        else if (opcao == 2) {
+            if (topo >= 0) {
+                std::cout << "Removido: " << pilha[topo] << std::endl;
+                topo--;
+            } else {
+                std::cout << "Pilha vazia!" << std::endl;
+            }
+        } 
+        else if (opcao == 3) {
+            if (topo >= 0) {
+                std::cout << "Pilha: ";
+                for (int i = 0; i <= topo; i++) {
+                    std::cout << pilha[i] << " ";
+                }
+                std::cout << std::endl;
+            } else {
+                std::cout << "Pilha vazia!" << std::endl;
+            }
+        } 
+        else if (opcao == 4) {
+            std::cout << "Saindo..." << std::endl;
+            break;
+        }
     }
     
     return 0;
