7fcaeacd624faaf7cbd02b9b4a64234f79eede12
profedufbarreto <01289211027@senacrs.edu.br>
2026-06-23 21:58:20 -0300

    foreach

--- Main.java
+++ Main.java
@@ -4,15 +4,13 @@ public class Main{
     public static void main(String[] args){
         Scanner teclado = new Scanner(System.in);
         
-        int soma = 0;
+        String[] frutas = {"Maçã", "Banana", "Uva", "Pera"};
 
-        int[] numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
-        for(int i = 0; i < numeros.length; i++){
-            soma += numeros[i];
+        for(String fruta : frutas){
+            System.out.println(fruta);
         }
 
-        System.out.println(soma);
+        
 
     }
 }
