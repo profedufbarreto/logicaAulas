300a4a4f86f14916d3c4460df1f4b83bbfdb1b2f
profedu_fbarreto <seu_email@github.com>
2026-06-24 15:14:38 -0300

    array

--- Main.java
+++ Main.java
@@ -1,16 +1,15 @@
 import java.util.Scanner;
 
-public class Main{
-    public static void main(String[] args){
+public class Main {
+    public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        
-        String[] frutas = {"Maçã", "Banana", "Uva", "Pera"};
 
-        for(String fruta : frutas){
-            System.out.println(fruta);
-        }
+        int[] numeros = { 10, 20, 30, 40, 50 };
 
-        
+        System.out.println("Primeiro elemento: " + numeros[0]);
+        System.out.println("Segundo elemento: " + numeros[1]);
+        System.out.println("Terceiro elemento: " + numeros[2]);
 
+        teclado.close();
     }
 }
