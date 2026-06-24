33788c3ab2e5ecd68aafc669456f5a9ee3d63c8f
profedufbarreto <01289211027@senacrs.edu.br>
2026-06-23 21:56:41 -0300

    array em java

--- Main.java
+++ Main.java
@@ -1,75 +1,18 @@
-import java.sql.*;
 import java.util.Scanner;
 
 public class Main{
-    static final String URL = "jdbc:mysql://localhost:3306/teste";
-    static final String USER = "root";
-    static final String PASSWORD = "";
-
     public static void main(String[] args){
-        Connection conn = null;
         Scanner teclado = new Scanner(System.in);
-        teclado.useLocale(java.util.Locale.US);
-
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection(URL, USER, PASSWORD);
-            System.out.println("Conectado!\n");
-
-            int opcao = 0;
-            do{
-                System.out.println("1 - Cadastrar");
-                System.out.println("2 - Ver todos");
-                System.out.println("3 - Sair");
-                System.out.println("Opção: ");
-                opcao = teclado.nextInt();
-
-                if(opcao == 1){
-                    cadastrar(teclado, conn);
-                }else if(opcao == 2){
-                    verTodos(conn);
-                }
-            }while(opcao != 3);
-            conn.close();
-        }catch(Exception e){
-            System.out.println("Erro: " + e.getMessage());
-        }
         
-    }
+        int soma = 0;
 
-    static void cadastrar(Scanner teclado, Connection conn) throws SQLException{
-        teclado.nextLine();
+        int[] numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-        System.out.println("Nome do carro: ");
-        String nome = teclado.nextLine();
-
-        System.out.println("Marca: ");
-        String marca = teclado.nextLine();
-
-        String sql = "INSERT INTO carros(nome, marca) VALUES (?, ?)";
-        PreparedStatement pstmt = conn.prepareStatement(sql);
-        pstmt.setString(1, nome);
-        pstmt.setString(2, marca);
-        pstmt.executeUpdate();
-        pstmt.close();
-
-        System.out.println("Cadastrado\n");
-
-    }
-
-    static void verTodos(Connection conn) throws SQLException{
-        String sql = "SELECT * FROM carros";
-        Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery(sql);
-
-        System.out.println("\n--- CARROS ---");
-        while(rs.next()){
-            int id = rs.getInt("id");
-            String nome = rs.getString("nome");
-            String marca = rs.getString("marca");
-            System.out.println("ID: " + id + " | Nome: " + nome + " | Marca: " + marca);
+        for(int i = 0; i < numeros.length; i++){
+            soma += numeros[i];
         }
-        System.out.println();
-        stmt.close();
+
+        System.out.println(soma);
+
     }
 }
--- main.cpp
+++ main.cpp
@@ -1,37 +1,44 @@
 #include <iostream>
 
 int main() {
-    int numeros[] = {15, 8, 25, 3, 42, 12, 7, 30};
-    int tamanho = 8;
+    int matriz[3][4] = {
+        {10, 25, 5, 40},
+        {15, 8, 30, 12},
+        {20, 3, 18, 35}
+    };
     
-    int maior = numeros[0];
-    int menor = numeros[0];
-    int posMaior = 0;
-    int posMenor = 0;
+    int maior = matriz[0][0];
+    int menor = matriz[0][0];
+    int linhaM = 0, colunaM = 0;
+    int linhaN = 0, colunaN = 0;
     
-    std::cout << "===== MIN E MAX =====" << std::endl;
+    std::cout << "===== MIN E MAX EM MATRIZ =====" << std::endl;
     std::cout << std::endl;
     
-    std::cout << "Array: ";
-    for (int i = 0; i < tamanho; i++) {
-        std::cout << numeros[i] << " ";
-    }
-    std::cout << std::endl;
-    std::cout << std::endl;
-    
-    for (int i = 0; i < tamanho; i++) {
-        if (numeros[i] > maior) {
-            maior = numeros[i];
-            posMaior = i;
+    std::cout << "Matriz:" << std::endl;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            std::cout << matriz[i][j] << " ";
+            
+            if (matriz[i][j] > maior) {
+                maior = matriz[i][j];
+                linhaM = i;
+                colunaM = j;
+            }
+            if (matriz[i][j] < menor) {
+                menor = matriz[i][j];
+                linhaN = i;
+                colunaN = j;
+            }
         }
-        if (numeros[i] < menor) {
-            menor = numeros[i];
-            posMenor = i;
-        }
+        std::cout << std::endl;
     }
     
-    std::cout << "MAIOR: " << maior << " na posição " << posMaior << std::endl;
-    std::cout << "MENOR: " << menor << " na posição " << posMenor << std::endl;
+    std::cout << std::endl;
+    std::cout << "MAIOR: " << maior << " na posição [" << linhaM 
+              << "][" << colunaM << "]" << std::endl;
+    std::cout << "MENOR: " << menor << " na posição [" << linhaN 
+              << "][" << colunaN << "]" << std::endl;
     
     return 0;
 }
