fbfc664cad39e4f14a1e6ce7cc20b7adf34c8dd0
profedu_fbarreto <seu_email@github.com>
2026-06-24 15:21:59 -0300

    encontrado valor dentro de um array

--- Main.java
+++ Main.java
@@ -5,9 +5,21 @@ public class Main {
         Scanner teclado = new Scanner(System.in);
 
         int[] numeros = { 10, 20, 30, 40, 50 };
+        int procurado = 30;
 
-        for (int num : numeros) {
-            System.out.println("Posição: " + num);
+        boolean encontrado = false;
+
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] == procurado) {
+                encontrado = true;
+                break;
+            }
+        }
+
+        if (encontrado) {
+            System.out.println("Número " + procurado + " Encontrado!!");
+        } else {
+            System.out.println("Número: " + procurado + " Não encontrado!!");
         }
 
         teclado.close();
