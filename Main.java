efadb61fb1c45974c52b8266c185008d231270db
profedu_fbarreto <seu_email@github.com>
2026-06-27 19:45:47 -0300

    average

--- Main.java
+++ Main.java
@@ -2,12 +2,14 @@ public class Main {
     public static void main(String[] args) {
 
         int[] numbers = { 10, 20, 30, 40, 50 };
+        int average = 0;
         int sum = 0;
 
         for (int i = 0; i < numbers.length; i++) {
             sum += numbers[i];
         }
+        average = sum / numbers.length;
 
-        System.out.println("Sum: " + sum);
+        System.out.println("Average: " + average);
     }
 }
