a6ec536ab901f6449fb1bbf654f573b36d76e26a
profedu_fbarreto <seu_email@github.com>
2026-06-27 19:37:21 -0300

    less

--- Main.java
+++ Main.java
@@ -2,13 +2,13 @@ public class Main {
     public static void main(String[] args) {
 
         int[] numbers = { 10, 50, 30, 40, 20 };
-        int bigger = numbers[0];
+        int less = numbers[0];
 
         for (int i = 0; i < numbers.length; i++) {
-            if (bigger < numbers[i]) {
-                bigger = numbers[i];
+            if (less > numbers[i]) {
+                less = numbers[i];
             }
         }
-        System.out.println("Bigger: " + bigger);
+        System.out.println("Less: " + less);
     }
 }
