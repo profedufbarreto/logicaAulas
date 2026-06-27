fbc443fa7c3084ebb3e872fc67302b8f276eca71
profedu_fbarreto <seu_email@github.com>
2026-06-27 19:51:36 -0300

    ímpares

--- Main.java
+++ Main.java
@@ -2,14 +2,14 @@ public class Main {
     public static void main(String[] args) {
 
         int[] numeros = { 10, 21, 30, 45, 50 };
-        int pares = 0;
+        int impares = 0;
 
         for (int i = 0; i < numeros.length; i++) {
-            if (numeros[i] % 2 == 0) {
-                pares += 1;
+            if (numeros[i] % 2 != 0) {
+                impares += 1;
             }
         }
 
-        System.out.println(pares);
+        System.out.println(impares);
     }
 }
