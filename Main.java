a57a60068475b6adacc6b9aa7f0fe35ff10c86e9
profedu_fbarreto <seu_email@github.com>
2026-06-27 19:35:50 -0300

    bigger

--- Main.java
+++ Main.java
@@ -1,9 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        int[] numeros = { 100, 200, 300, 400, 500 };
+        int[] numbers = { 10, 50, 30, 40, 20 };
+        int bigger = numbers[0];
 
-        System.out.println(numeros[numeros.length - 1]);
-
+        for (int i = 0; i < numbers.length; i++) {
+            if (bigger < numbers[i]) {
+                bigger = numbers[i];
+            }
+        }
+        System.out.println("Bigger: " + bigger);
     }
 }
