3eb620fa9e85b65ce1a7f7c4536bcd4871ecc231
profedu_fbarreto <seu_email@github.com>
2026-06-27 19:41:07 -0300

    sum

--- Main.java
+++ Main.java
@@ -1,14 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        int[] numbers = { 10, 50, 30, 40, 20 };
-        int less = numbers[0];
+        int[] numbers = { 10, 20, 30, 40, 50 };
+        int sum = 0;
 
         for (int i = 0; i < numbers.length; i++) {
-            if (less > numbers[i]) {
-                less = numbers[i];
-            }
+            sum += numbers[i];
         }
-        System.out.println("Less: " + less);
+
+        System.out.println("Sum: " + sum);
     }
 }
