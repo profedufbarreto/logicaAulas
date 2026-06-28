ab5db90ebe8a6b0b637255a9c5506d057649e6c9
profedu_fbarreto <seu_email@github.com>
2026-06-28 18:58:44 -0300

    arraylist

--- main.cpp
+++ main.cpp
@@ -1,44 +1,22 @@
 #include <iostream>
+#include <vector> // Importa o equivalente ao ArrayList
 
 int main() {
-    int matriz[3][4] = {
-        {10, 25, 5, 40},
-        {15, 8, 30, 12},
-        {20, 3, 18, 35}
-    };
-    
-    int maior = matriz[0][0];
-    int menor = matriz[0][0];
-    int linhaM = 0, colunaM = 0;
-    int linhaN = 0, colunaN = 0;
-    
-    std::cout << "===== MIN E MAX EM MATRIZ =====" << std::endl;
-    std::cout << std::endl;
-    
-    std::cout << "Matriz:" << std::endl;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 4; j++) {
-            std::cout << matriz[i][j] << " ";
-            
-            if (matriz[i][j] > maior) {
-                maior = matriz[i][j];
-                linhaM = i;
-                colunaM = j;
-            }
-            if (matriz[i][j] < menor) {
-                menor = matriz[i][j];
-                linhaN = i;
-                colunaN = j;
-            }
-        }
-        std::cout << std::endl;
+    // Equivalente a: ArrayList<Integer> numeros = new ArrayList<>();
+    std::vector<int> numeros;
+
+    // Equivalente ao numeros.add() do Java:
+    numeros.push_back(10);
+    numeros.push_back(20);
+    numeros.push_back(30);
+
+    // No C++ não dá para printar o vetor inteiro direto com cout << numeros;
+    // Precisamos de um loop rápido (foreach) para mostrar os valores:
+    std::cout << "[";
+    for (int num : numeros) {
+        std::cout << num << " ";
     }
-    
-    std::cout << std::endl;
-    std::cout << "MAIOR: " << maior << " na posição [" << linhaM 
-              << "][" << colunaM << "]" << std::endl;
-    std::cout << "MENOR: " << menor << " na posição [" << linhaN 
-              << "][" << colunaN << "]" << std::endl;
-    
+    std::cout << "]" << std::endl;
+
     return 0;
 }
