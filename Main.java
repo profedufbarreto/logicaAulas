bb71508a0187d32706df1de804a6ed4ccecdf1f6
profedu_fbarreto <seu_email@github.com>
2026-06-28 11:00:02 -0300

    encontrar posição de um elemento

--- Main.java
+++ Main.java
@@ -1,10 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
-        int[] numeros = { 100, 200, 300, 400, 500 };
+        int[] numeros = { 5, 15, 25, 35, 45, 55, 65 };
+        int posicao = -1;
+        int num = 35;
 
         for (int i = 0; i < numeros.length; i++) {
-            System.out.println("Posição: " + i + ": " + numeros[i]);
+            if (numeros[i] == num) {
+                posicao = i;
+                break;
+            }
+
+        }
+
+        if (posicao != -1) {
+            System.out.println("Encontrado na posição: " + posicao);
+        } else {
+            System.out.println("Não encontrado!");
         }
 
     }
