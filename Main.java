c0d04cd387cf8d6e1034b607c940079bcd95f6af
profedu_fbarreto <seu_email@github.com>
2026-06-28 17:43:11 -0300

    soma dos ímpares

--- Main.java
+++ Main.java
@@ -2,14 +2,13 @@ public class Main {
     public static void main(String[] args) {
 
         int[] numeros = { 10, 21, 30, 45, 50, 8, 33, 14 };
-        int somarPar = 0;
+        int somarImpares = 0;
 
         for (int i = 0; i < numeros.length; i++) {
-            if (numeros[i] % 2 == 0) {
-                somarPar += numeros[i];
+            if (numeros[i] % 2 != 0) {
+                somarImpares += numeros[i];
             }
         }
-
-        System.out.println("O resultado da soma dos pares é: " + somarPar);
+        System.out.println("A soma dos ímpares é: " + somarImpares);
     }
 }
