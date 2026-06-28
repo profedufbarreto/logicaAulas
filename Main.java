eaeaf19b72ee21088317dcd1f13a6cc14fa141de
profedu_fbarreto <seu_email@github.com>
2026-06-28 10:26:45 -0300

    maior, menor, soma

--- Main.java
+++ Main.java
@@ -1,15 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
-        int[] numeros = { 10, 21, 30, 45, 50 };
-        int impares = 0;
+        int[] numeros = { 10, 21, 30, 45, 50, 8, 33, 14 };
+        int maior = numeros[0];
+        int menor = numeros[0];
+        int soma = 0;
 
         for (int i = 0; i < numeros.length; i++) {
-            if (numeros[i] % 2 != 0) {
-                impares += 1;
+            if (maior < numeros[i]) {
+                maior = numeros[i];
             }
+            if (menor > numeros.length) {
+                menor = numeros[i];
+            }
+            soma += numeros[i];
         }
-
-        System.out.println(impares);
+        System.out.println("O maior número é: " + maior);
+        System.out.println("O menos número é: " + menor);
+        System.out.println("A soma de todos elementos é: " + soma);
     }
 }
