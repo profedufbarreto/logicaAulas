2bd94a316119c8e866590eb480575f172cab7709
profedu_fbarreto <seu_email@github.com>
2026-06-28 10:37:16 -0300

    maior, menor, soma, média, pares, ímpares

--- Main.java
+++ Main.java
@@ -5,6 +5,9 @@ public class Main {
         int maior = numeros[0];
         int menor = numeros[0];
         int soma = 0;
+        double media = 0;
+        int pares = 0;
+        int impares = 0;
 
         for (int i = 0; i < numeros.length; i++) {
             if (maior < numeros[i]) {
@@ -13,10 +16,20 @@ public class Main {
             if (menor > numeros.length) {
                 menor = numeros[i];
             }
+            if (numeros[i] % 2 == 0) {
+                pares += 1;
+            }
+            if (numeros[i] % 2 != 0) {
+                impares += 1;
+            }
             soma += numeros[i];
+            media = soma / numeros.length;
         }
         System.out.println("O maior número é: " + maior);
         System.out.println("O menos número é: " + menor);
         System.out.println("A soma de todos elementos é: " + soma);
+        System.out.println("A média dos elementos é: " + media);
+        System.out.println("Quantidade de números pares: " + pares);
+        System.out.println("Quantidade de números ímpares: " + impares);
     }
 }
