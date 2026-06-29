7033d9edc28a267114d64c23b16d485567d8e892
profedu_fbarreto <seu_email@github.com>
2026-06-29 14:18:44 -0300

    soma com arraylist

--- Main.java
+++ Main.java
@@ -11,22 +11,11 @@ public class Main {
         numeros.add(40);
         numeros.add(50);
 
-        System.out.println("Lista original: " + numeros);
-        System.out.println("Tamanho: " + numeros.size());
+        int soma = 0;
 
-        System.out.println("\nPrimeiro elemento: " + numeros.get(0));
-        System.out.println("Último elemtno: " + numeros.get(numeros.size() - 1));
-
-        System.out.println("\nContém 30? " + numeros.contains(30));
-        System.out.println("Contém 99? " + numeros.contains(99));
-
-        numeros.remove(2);
-        System.out.println("\nDepois de remover posição 2: " + numeros);
-
-        System.out.println("\nTodos os elementos: ");
-        for (int numero : numeros) {
-            System.out.println("- " + numero);
+        for (int i = 0; i < numeros.size(); i++) {
+            soma += numeros.get(i);
         }
-
+        System.out.println("O resultado da soma é: " + soma);
     }
 }
