00360c1ed4d76231ee19290264fa5edf81219d76
profedufbarreto <01289211027@senacrs.edu.br>
2026-06-30 19:49:58 -0300

    ler e exibir num array

--- main.cpp
+++ main.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
-#include <vector> // Importa o equivalente ao ArrayList
 
 int main() {
-    // Equivalente a: ArrayList<Integer> numeros = new ArrayList<>();
-    std::vector<int> numeros;
-
-    // Equivalente ao numeros.add() do Java:
-    numeros.push_back(10);
-    numeros.push_back(20);
-    numeros.push_back(30);
-
-    // No C++ não dá para printar o vetor inteiro direto com cout << numeros;
-    // Precisamos de um loop rápido (foreach) para mostrar os valores:
-    std::cout << "[";
-    for (int num : numeros) {
-        std::cout << num << " ";
+    int numeros[5];
+    
+    std::cout << "===== 1. LER E EXIBIR =====" << std::endl;
+    std::cout << std::endl;
+    
+    for (int i = 0; i < 5; i++) {
+        std::cout << "Digite o numero " << (i + 1) << ": ";
+        std::cin >> numeros[i];
     }
-    std::cout << "]" << std::endl;
-
+    
+    std::cout << std::endl;
+    std::cout << "Números digitados: ";
+    for (int i = 0; i < 5; i++) {
+        std::cout << numeros[i] << " ";
+    }
+    std::cout << std::endl;
+    
     return 0;
 }
