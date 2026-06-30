b517dbc49a05c674f7d83faa2b280fbc02cf1166
profedufbarreto <01289211027@senacrs.edu.br>
2026-06-30 20:52:39 -0300

    calcular média

--- main.cpp
+++ main.cpp
@@ -2,26 +2,21 @@
 
 int main() {
     int numeros[5];
-    int menor = 0;
+    int soma = 0;
     
-    std::cout << "===== 4. ENCONTRAR MENOR =====" << std::endl;
+    std::cout << "===== 5. CALCULAR MEDIA =====" << std::endl;
     std::cout << std::endl;
     
     for (int i = 0; i < 5; i++) {
         std::cout << "Digite o numero " << (i + 1) << ": ";
         std::cin >> numeros[i];
+        soma += numeros[i];
     }
     
-    menor = numeros[0];
-    
-    for (int i = 1; i < 5; i++) {
-        if (numeros[i] < menor) {
-            menor = numeros[i];
-        }
-    }
+    float media = soma / 5.0;
     
     std::cout << std::endl;
-    std::cout << "O menor numero eh: " << menor << std::endl;
+    std::cout << "Media: " << media << std::endl;
     
     return 0;
 }
