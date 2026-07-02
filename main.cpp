d85dd36bcf297592172a3641b1d8ba45d54de309
profedufbarreto <01289211027@senacrs.edu.br>
2026-07-02 20:42:51 -0300

    remover elemento específico

--- main.cpp
+++ main.cpp
@@ -4,15 +4,23 @@
 int main() {
     std::vector<int> numeros;
     
-    std::cout << "===== 2. TAMANHO DO VECTOR =====" << std::endl;
+    std::cout << "===== 6. REMOVER ELEMENTO ESPECIFICO =====" << std::endl;
     std::cout << std::endl;
     
-    numeros.push_back(5);
     numeros.push_back(10);
-    numeros.push_back(15);
+    numeros.push_back(20);
+    numeros.push_back(30);
+    numeros.push_back(40);
     
-    std::cout << "Tamanho do vector: " << numeros.size() << std::endl;
-    std::cout << "Elementos: ";
+    std::cout << "Antes: ";
+    for (int i = 0; i < numeros.size(); i++) {
+        std::cout << numeros[i] << " ";
+    }
+    std::cout << std::endl;
+    
+    numeros.erase(numeros.begin() + 1);
+    
+    std::cout << "Depois: ";
     for (int i = 0; i < numeros.size(); i++) {
         std::cout << numeros[i] << " ";
     }
