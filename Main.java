f9b02d221e99377ba5f2bb17c9ef89194c0a7123
Eduardo Barreto <fbarreto.eduardo@gmail.com>
2026-07-04 11:02:05 -0300

    procurar número

--- Main.java
+++ Main.java
@@ -6,23 +6,25 @@ public class Main {
         ArrayList<Integer> numeros = new ArrayList<>();
 
         numeros.add(10);
-        numeros.add(21);
+        numeros.add(20);
         numeros.add(30);
-        numeros.add(45);
+        numeros.add(40);
         numeros.add(50);
-        numeros.add(8);
-        numeros.add(33);
-        numeros.add(14);
 
         int posicao = -1;
+        int procurar = 30;
 
-        for(int i = 0; i < numeros.size(); i++){
-            if(numeros.get(i) == 30){
+        for (int i = 0; i < numeros.size(); i++) {
+            if (numeros.get(i) == procurar) {
                 posicao = i;
+                break;
             }
         }
-
-        System.out.println("A posição do 30 é: " + posicao);
+        if (posicao != -1) {
+            System.out.println("Encontrado na posição: " + posicao);
+        } else {
+            System.out.println("Não encontrado!");
+        }
 
     }
 }
