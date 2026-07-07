191270fe7e42ab327a925b09ef4d45ed42049762
profedufbarreto <01289211027@senacrs.edu.br>
2026-07-07 19:41:56 -0300

    encontrado e posicao

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 int main() {
     std::vector<int> numeros;
     
-    std::cout << "===== 10. SOMA TOTAL =====" << std::endl;
+    std::cout << "===== 13. BUSCAR ELEMENTO =====" << std::endl;
     std::cout << std::endl;
     
     numeros.push_back(10);
@@ -19,12 +19,21 @@ int main() {
     std::cout << std::endl;
     std::cout << std::endl;
     
-    int soma = 0;
+    int buscado = 30;
+    int posicao = -1;
+    
     for (int i = 0; i < numeros.size(); i++) {
-        soma += numeros[i];
+        if (numeros[i] == buscado) {
+            posicao = i;
+            break;
+        }
     }
     
-    std::cout << "Soma: " << soma << std::endl;
+    if (posicao != -1) {
+        std::cout << "Numero " << buscado << " encontrado na posicao " << posicao << std::endl;
+    } else {
+        std::cout << "Numero nao encontrado!" << std::endl;
+    }
     
     return 0;
 }
