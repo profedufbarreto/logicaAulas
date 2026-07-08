738fb25dc6110c602cfeb5d742d6ca127c7fbf26
profedufbarreto <01289211027@senacrs.edu.br>
2026-07-07 21:29:35 -0300

    vector invertido

--- main.cpp
+++ main.cpp
@@ -4,35 +4,24 @@
 int main() {
     std::vector<int> numeros;
     
-    std::cout << "===== 15. PARES E IMPARES =====" << std::endl;
+    std::cout << "===== 16. INVERTER VECTOR =====" << std::endl;
     std::cout << std::endl;
     
-    numeros.push_back(5);
     numeros.push_back(10);
-    numeros.push_back(15);
     numeros.push_back(20);
-    numeros.push_back(25);
+    numeros.push_back(30);
+    numeros.push_back(40);
+    numeros.push_back(50);
     
-    std::cout << "Vector: ";
+    std::cout << "Original: ";
     for (int i = 0; i < numeros.size(); i++) {
         std::cout << numeros[i] << " ";
     }
     std::cout << std::endl;
-    std::cout << std::endl;
     
-    std::cout << "Pares: ";
-    for (int i = 0; i < numeros.size(); i++) {
-        if (numeros[i] % 2 == 0) {
-            std::cout << numeros[i] << " ";
-        }
-    }
-    std::cout << std::endl;
-    
-    std::cout << "Impares: ";
-    for (int i = 0; i < numeros.size(); i++) {
-        if (numeros[i] % 2 != 0) {
-            std::cout << numeros[i] << " ";
-        }
+    std::cout << "Invertido: ";
+    for (int i = numeros.size() - 1; i >= 0; i--) {
+        std::cout << numeros[i] << " ";
     }
     std::cout << std::endl;
     
