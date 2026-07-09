22eba7ac13d2a1619c071289ea71349a9f412641
profedufbarreto <01289211027@senacrs.edu.br>
2026-07-09 19:15:38 -0300

    entrada e saída

--- main.cpp
+++ main.cpp
@@ -1,29 +1,14 @@
 #include <iostream>
-#include <vector>
+#include <string>
 
 int main() {
-    std::vector<int> numeros;
+    std::string palavra;
     
-    std::cout << "===== 16. INVERTER VECTOR =====" << std::endl;
-    std::cout << std::endl;
+    std::cout << "Digite uma palavra: ";
+    std::cin >> palavra;
     
-    numeros.push_back(10);
-    numeros.push_back(20);
-    numeros.push_back(30);
-    numeros.push_back(40);
-    numeros.push_back(50);
-    
-    std::cout << "Original: ";
-    for (int i = 0; i < numeros.size(); i++) {
-        std::cout << numeros[i] << " ";
-    }
-    std::cout << std::endl;
-    
-    std::cout << "Invertido: ";
-    for (int i = numeros.size() - 1; i >= 0; i--) {
-        std::cout << numeros[i] << " ";
-    }
-    std::cout << std::endl;
+    std::cout << "A palavra digitada possui o caractere inicial: " << palavra[0] << std::endl;
+    std::cout << "Resultado: " << palavra << std::endl;
     
     return 0;
 }
