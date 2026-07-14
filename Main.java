cee33e5b4f29b34a54a8f802d45fd1d1bf06a8b7
Eduardo Barreto <fbarreto.eduardo@gmail.com>
2026-07-14 11:46:58 -0300

    funções

--- Main.java
+++ Main.java
@@ -1,23 +1,11 @@
-import java.util.ArrayList;
-
 public class Main {
+    public static int dobrar(int numero) {
+        return numero * 2;
+    }
+
     public static void main(String[] args) {
+        int resultado = dobrar(5);
 
-        ArrayList<Integer> numeros = new ArrayList<>();
-
-        numeros.add(10);
-        numeros.add(20);
-        numeros.add(30);
-        numeros.add(40);
-        numeros.add(50);
-
-        ArrayList<Integer> copia = new ArrayList<>();
-
-        for (int i = 0; i < numeros.size(); i++) {
-            copia.add(numeros.get(i));
-            copia.add(numeros.get(i));
-        }
-
-        System.out.println("Resultado: " + copia);
+        System.out.println("O número é: " + resultado);
     }
 }
