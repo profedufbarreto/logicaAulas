20a22851a1f07dbb788092df612e3e98a2bf55c7
Eduardo Barreto <fbarreto.eduardo@gmail.com>
2026-07-14 11:44:52 -0300

    repetir arraylist

--- Main.java
+++ Main.java
@@ -5,22 +5,19 @@ public class Main {
 
         ArrayList<Integer> numeros = new ArrayList<>();
 
-        int posicao = -1;
-
         numeros.add(10);
         numeros.add(20);
         numeros.add(30);
         numeros.add(40);
         numeros.add(50);
 
-        System.out.println("Antes: " + numeros);
+        ArrayList<Integer> copia = new ArrayList<>();
 
         for (int i = 0; i < numeros.size(); i++) {
-            if (numeros.get(i) == 30) {
-                numeros.remove(i);
-            }
+            copia.add(numeros.get(i));
+            copia.add(numeros.get(i));
         }
 
-        System.out.println("Depois: " + numeros);
+        System.out.println("Resultado: " + copia);
     }
 }
