9751e7a65631a26c0a7f42a835685299adf3a423
Eduardo Barreto <fbarreto.eduardo@gmail.com>
2026-07-14 12:03:23 -0300

    média

--- Main.java
+++ Main.java
@@ -1,13 +1,30 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int somar(int a, int b) {
-        return a + b;
+    public static double media(double nota1, double nota2, double nota3) {
+        double soma = nota1 + nota2 + nota3;
+        return soma / 3;
     }
 
     public static void main(String[] args) {
-        int resultado = somar(4, 2);
+        Scanner teclado = new Scanner(System.in);
 
-        System.out.println("O resultado é: " + resultado);
+        System.out.println("Digite a primeira nota: ");
+        double res1 = teclado.nextDouble();
+        teclado.nextLine();
+
+        System.out.println("Digite a segunda nota: ");
+        double res2 = teclado.nextDouble();
+        teclado.nextLine();
+
+        System.out.println("Digite a terceira nota: ");
+        double res3 = teclado.nextDouble();
+        teclado.nextLine();
+
+        double resultado = media(res1, res2, res3);
+
+        System.out.println("A média final foi: " + resultado);
+
+        teclado.close();
     }
 }
