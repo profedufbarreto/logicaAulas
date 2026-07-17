4e3bf52e002bb802321215341fe922c75e2dbf46
Eduardo Barreto <fbarreto.eduardo@gmail.com>
2026-07-17 10:19:51 -0300

    positivo

--- Main.java
+++ Main.java
@@ -1,33 +1,24 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int maior(int a, int b, int c) {
-        int maiorNumero = a;
+    public static boolean positivo(int num) {
+        boolean pos = false;
 
-        if (b > maiorNumero) {
-            maiorNumero = b;
+        if (num >= 0) {
+            pos = true;
         }
-        if (c > maiorNumero) {
-            maiorNumero = c;
-        }
-        return maiorNumero;
+        return pos;
     }
 
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
-        System.out.println("Digite um valor: ");
+        System.out.println("Digite um número: ");
         int num1 = teclado.nextInt();
 
-        System.out.println("Digite o segundo valor: ");
-        int num2 = teclado.nextInt();
+        boolean resultado = positivo(num1);
 
-        System.out.println("Digite um terceiro número: ");
-        int num3 = teclado.nextInt();
-
-        int resultado = maior(num1, num2, num3);
-
-        System.out.println("O maior é: " + resultado);
+        System.out.println("O número é positivo? " + resultado);
 
         teclado.close();
     }
