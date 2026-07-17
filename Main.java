1e18bd30dc77e56c3860e1319290a90b58d175ef
Eduardo Barreto <fbarreto.eduardo@gmail.com>
2026-07-17 10:13:59 -0300

    maior

--- Main.java
+++ Main.java
@@ -1,22 +1,33 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int somar(int a, int b) {
-        return a + b;
+    public static int maior(int a, int b, int c) {
+        int maiorNumero = a;
+
+        if (b > maiorNumero) {
+            maiorNumero = b;
+        }
+        if (c > maiorNumero) {
+            maiorNumero = c;
+        }
+        return maiorNumero;
     }
 
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
-        System.out.println("Digite um valor para num1: ");
+        System.out.println("Digite um valor: ");
         int num1 = teclado.nextInt();
 
-        System.out.println("Digite um valor para num2: ");
+        System.out.println("Digite o segundo valor: ");
         int num2 = teclado.nextInt();
 
-        int resultado = somar(num1, num2);
+        System.out.println("Digite um terceiro número: ");
+        int num3 = teclado.nextInt();
 
-        System.out.println("O resultado é: " + resultado);
+        int resultado = maior(num1, num2, num3);
+
+        System.out.println("O maior é: " + resultado);
 
         teclado.close();
     }
