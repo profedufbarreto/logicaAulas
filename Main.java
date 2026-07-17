ff964bfb28599794575996c1ef84ecfc833038b9
Eduardo Barreto <fbarreto.eduardo@gmail.com>
2026-07-17 09:55:36 -0300

    soma com function

--- Main.java
+++ Main.java
@@ -1,17 +1,22 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int doble(int num) {
-        return num * 2;
+    public static int somar(int a, int b) {
+        return a + b;
     }
 
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
-        System.out.println("Digite um número para ter o dobro: ");
-        int a = teclado.nextInt();
+        System.out.println("Digite um valor para num1: ");
+        int num1 = teclado.nextInt();
 
-        System.out.println("O número dobrado é: " + doble(a));
+        System.out.println("Digite um valor para num2: ");
+        int num2 = teclado.nextInt();
+
+        int resultado = somar(num1, num2);
+
+        System.out.println("O resultado é: " + resultado);
 
         teclado.close();
     }
